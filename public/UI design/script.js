ad5fc2289ba3fccc6a27a0388c00ecadc3d4bedd
Joseph Ng <127112633+Phandastick@users.noreply.github.com>
2024-10-14 22:55:41 +0800

    updated script

---
+++ public/UI design/script.js
@@ -0,0 +1,13 @@
+const preset_menu = [
+    {
+        1: 'Home',
+        2: 'Upcoming',
+        3: 'Filters'
+    }
+]
+const sidebar_menu = document.getElementsByClassName('sidebar')
+
+preset_menu[0].forEach(item => {
+    let sidebar_item = document.createElement('div');
+    
+});
