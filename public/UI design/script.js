fcb569a3f762519f75f6a4e1e3ab4fae5a65d266
Phandastick <127112633+Phandastick@users.noreply.github.com>
2024-10-16 16:12:40 +0800

    Added icons and color palatte
    Added variable css colors to implement a scalable color scheme
    Added SVG files in src/assets for icons

--- public/UI design/script.js
+++ public/UI design/script.js
@@ -1,21 +1,60 @@
-
-const preset_menu = [
-    {
-        1: 'Home',
-        2: 'Upcoming',
-        3: 'Filters'
+const preset_menu = {
+    'Home':'/src/assets/home.svg', 
+    'Upcoming':'/src/assets/upcoming.svg',
+    'Filters':'/src/assets/filters.svg'
     }
-]
-const sidebar_menu = document.getElementsByClassName('sidebar')
 
-preset_menu[0].forEach(item => {
-    let sidebar_item = document.createElement('div');
-    
-});
 
+const sidebar_menu = document.getElementsByClassName('sidebar-presets')[0]
+const siderbar_custom = document.getElementsByClassName('sidebar-custom')[0];
 
 const todaydate = new Date();
 const dates = document.getElementsByClassName('navbar-dates');
+
+function initSidebar(){
+    var idIndex = 1;
+    for (var text in preset_menu) {
+        let new_sidebar_item = document.createElement('div');
+        new_sidebar_item.id = 'sidebar-preset-' + idIndex;
+        new_sidebar_item.classList.add('sidebar-item')
+
+        let new_sidebar_item_icon = document.createElement('EMBED');
+        new_sidebar_item.appendChild(new_sidebar_item_icon);
+        new_sidebar_item_icon.classList.add('sidebar-preset-' + idIndex)
+        new_sidebar_item_icon.classList.add('sidebar-item-icon')
+        new_sidebar_item_icon.src = preset_menu[text];
+        
+        let new_sidebar_item_text = document.createElement('a');
+        new_sidebar_item.appendChild(new_sidebar_item_text);
+        new_sidebar_item_text.classList.add('sidebar-preset-' + idIndex);
+        new_sidebar_item_text.classList.add('sidebar-item-text');
+        new_sidebar_item_text.text = text;
+
+        sidebar_menu.appendChild(new_sidebar_item);
+        idIndex++
+    }
+}
+
+function addSidebarItem(name, icon){
+    let new_sidebar_item = document.createElement('div');
+    new_sidebar_item.id = 'sidebar-preset-' + idIndex;
+    new_sidebar_item.classList.add('sidebar-item')
+
+    let new_sidebar_item_icon = document.createElement('EMBED');
+    new_sidebar_item.appendChild(new_sidebar_item_icon);
+    new_sidebar_item_icon.classList.add('sidebar-preset-' + idIndex)
+    new_sidebar_item_icon.classList.add('sidebar-item-icon')
+    new_sidebar_item_icon.src = preset_menu[text];
+    
+    let new_sidebar_item_text = document.createElement('a');
+    new_sidebar_item.appendChild(new_sidebar_item_text);
+    new_sidebar_item_text.classList.add('sidebar-preset-' + idIndex);
+    new_sidebar_item_text.classList.add('sidebar-item-text');
+    new_sidebar_item_text.text = text;
+
+    sidebar_menu.appendChild(new_sidebar_item);
+    idIndex++
+}
 
 async function initDateList(){
     let today = todaydate.getDate();
@@ -29,6 +68,7 @@ async function initDateList(){
     // console.log(dateList);
 }
 
+// ADD CODE HERE IF NEED TASHA
 
 document.addEventListener('DOMContentLoaded', () => {
     initDateList().then(
@@ -38,4 +78,5 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
     );
+    initSidebar();
 })
