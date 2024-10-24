d71d1c730878180eded494c87e9d9655a7b78d1a
Phandastick <127112633+Phandastick@users.noreply.github.com>
2024-10-24 15:49:47 +0800

    Commit -update added dates to navbar
    - Added function to calculate the dates of the week from current date

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,17 +1,44 @@
-function Navbar () {
-    const today = new Date().getDate();
+function getDateList(){
+    const date = new Date()
+    var today = date.getDate();
+    var weekday = date.getDay();
     var dateList = []; 
 
+    today = today-weekday
+    date.setDate(today)
+    // console.log(date.getDate())
+
+    date.setDate(date.getDate()+6)
+    // console.log(date.getDate())
+    
     for(var i = 0; i < 7; i++){
-        dateList[i] = today-3+i;
+        dateList[i] = today+i;
     }
+
+    console.log(dateList)
+    return dateList
+}
+
+function Navbar () {
+    const dateList = getDateList()
+    let today = new Date().getDate();
+    let navbarClass;
+
     return (
         <div className="navbar header">
             {
-            dateList.map((date) => {
-                    <div className="navbar-item">
-                        <a className="navbar-dates">date</a>
-                    </div>
+                dateList.map((dateNum) => {
+                    if(dateNum == today){
+                        navbarClass = 'navbar-item-today';
+                    } else {
+                        navbarClass = 'navbar-item';
+                    }
+
+                    return (
+                        <div className = {navbarClass}>
+                            <a className="navbar-dates">{dateNum}</a>
+                        </div>
+                    )
                 })
             }
         </div>
