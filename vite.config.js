fc10533c312fa2dccdea1c19ae58ed28d9b47486
Phandastick <127112633+Phandastick@users.noreply.github.com>
2024-11-08 15:13:15 +0800

    Update Packages

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',   // Output directory for the build files
-    sourcemap: true,   // Generate source maps for debugging
-    minify: 'esbuild', // Minification tool to use
-  },
 })
