d232547119f97c2676cfc6baf2f8ca58d3026116
Joseph Ng <127112633+Phandastick@users.noreply.github.com>
2025-01-25 23:44:34 +0800

    Commit -update file structure

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
-import MainPage from "./app/components/MainPage/MainPage";
-import LoginPage from "./app/components/LoginPage/LoginPage";
-import { loginContext } from "./app/contexts/Contexts";
+import MainPage from "./components/MainPage/MainPage";
+import LoginPage from "./components/LoginPage/LoginPage";
+import { loginContext } from "./contexts/Contexts";
 
 //TODO: Add calendar mode
 
